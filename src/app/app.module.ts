de356e288911ff600268369f973db2a2c5351dbb
FRANCO EXEQUIEL SCHULER ALLUB <franschuler13@gmail.com>
2022-08-01 14:31:19 +0200

    Proyecto Angular base y rutas

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './modules/dashboard/pages/dashboard.component';
+import { HomeComponent } from './modules/dashboard/components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
